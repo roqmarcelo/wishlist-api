3f87f1cb1e41797051ee4ce4290bc2a651f0a01b
roqmarcelo <roque.santoro@gmail.com>
2019-11-24 23:49:12 -0300

    Aumento da cobertura dos testes

--- src/test/java/com/luizalabs/domain/customer/CustomerServiceTest.java
+++ src/test/java/com/luizalabs/domain/customer/CustomerServiceTest.java
@@ -1,9 +1,16 @@
 package com.luizalabs.domain.customer;
 
+import com.luizalabs.infrastructure.exception.EmailAlreadyRegisteredException;
+import com.luizalabs.infrastructure.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
@@ -16,5 +23,61 @@ class CustomerServiceTest {
     @BeforeEach
     void init() {
         customerService = new CustomerService(customerDAO);
+    }
+
+    @Test
+    void saveEmailAlreadyRegisteredException() {
+        when(customerDAO.existsBy("valid@email.com")).thenReturn(true);
+        Customer customer = new Customer(null, "Customer", "valid@email.com");
+        assertThrows(EmailAlreadyRegisteredException.class, () -> customerService.saveOrUpdate(customer),
+                "E-mail already registered.");
+    }
+
+    @Test
+    void updateNotFoundException() {
+        when(customerDAO.existsBy("valid@email.com")).thenReturn(true);
+        Customer customer = new Customer(1L, "Customer", "valid@email.com");
+        lenient().when(customerDAO.find(1L)).thenReturn(customer);
+        assertThrows(NotFoundException.class, () -> customerService.saveOrUpdate(customer),
+                "Customer not found.");
+    }
+
+    @Test
+    void updateEmailAlreadyRegisteredException() {
+        Customer existingCustomer  = new Customer(1L, "Existing Customer", "another@email.com");
+        Customer customer = new Customer(1L, "Customer", "valid@email.com");
+        when(customerDAO.find(1L)).thenReturn(existingCustomer);
+        when(customerDAO.existsBy("valid@email.com")).thenReturn(true);
+        assertThrows(EmailAlreadyRegisteredException.class, () -> customerService.saveOrUpdate(customer),
+                "E-mail already registered.");
+    }
+
+    @Test
+    void saveSuccess() {
+        Customer customer = new Customer(null, "Customer", "valid@email.com");
+        when(customerDAO.existsBy("valid@email.com")).thenReturn(false);
+        when(customerDAO.save(customer)).thenReturn(1L);
+        assertEquals(customerService.saveOrUpdate(customer), 1L);
+    }
+
+    @Test
+    void updateSuccess() {
+        Customer customer = new Customer(1L, "Customer", "valid@email.com");
+        when(customerDAO.find(1L)).thenReturn(customer);
+        when(customerDAO.existsBy("valid@email.com")).thenReturn(false);
+        when(customerDAO.update(customer)).thenReturn(true);
+        assertEquals(customerService.saveOrUpdate(customer), 1L);
+    }
+
+    @Test
+    void deleteSuccess() {
+        when(customerDAO.delete(1L)).thenReturn(true);
+        assertTrue(customerService.delete(1L));
+    }
+
+    @Test
+    void deleteFail() {
+        when(customerDAO.delete(1L)).thenReturn(false);
+        assertFalse(customerService.delete(1L));
     }
 }
--- src/test/java/com/luizalabs/domain/product/ProductServiceTest.java
+++ src/test/java/com/luizalabs/domain/product/ProductServiceTest.java
@@ -1,9 +1,20 @@
 package com.luizalabs.domain.product;
 
+import com.luizalabs.infrastructure.exception.NotFoundException;
+import com.luizalabs.infrastructure.exception.ProductAlreadyAddedException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -16,5 +27,56 @@ class ProductServiceTest {
     @BeforeEach
     void init() {
         productService = new ProductService(productDAO);
+    }
+
+    @Test
+    void listEmpty() {
+        assertTrue(productService.list(1L).isEmpty());
+    }
+
+    @Test
+    void listSuccess() {
+        Product product = new Product("1bf0f365-fbdd-4e21-9786-da459d78dd1f", "title", "image", 6000.0d, 10);
+        List<Product> products = new ArrayList<>(1);
+        products.add(product);
+        when(productDAO.list(1L)).thenReturn(products);
+        assertEquals(productService.list(1L).size(), 1);
+    }
+
+    @Test
+    void findNotFoundException() {
+        assertThrows(NotFoundException.class, () -> productService.find(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f"),
+                "Product not found.");
+    }
+
+    @Test
+    void findSuccess() {
+        Product product = new Product("1bf0f365-fbdd-4e21-9786-da459d78dd1f", "title", "image", 6000.0d, 10);
+        when(productDAO.find(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f")).thenReturn(product);
+        assertNotNull(productService.find(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f"));
+    }
+
+    @Test
+    void saveProductAlreadyAddedException() {
+        when(productDAO.exists(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f")).thenReturn(true);
+        assertThrows(ProductAlreadyAddedException.class, () -> productService.save(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f"),
+                "Product already added.");
+    }
+
+    @Test
+    void saveSuccess() {
+        productService.save(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f");
+    }
+
+    @Test
+    void deleteSuccess() {
+        when(productDAO.delete(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f")).thenReturn(true);
+        assertTrue(productService.delete(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f"));
+    }
+
+    @Test
+    void deleteFail() {
+        when(productDAO.delete(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f")).thenReturn(false);
+        assertFalse(productService.delete(1L, "1bf0f365-fbdd-4e21-9786-da459d78dd1f"));
     }
 }
